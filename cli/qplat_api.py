a19d17424b4aa0869698d50b83eace7bce91ed55
Hector Urroz <hurroz@tendrilinc.com>
2014-11-29 14:02:35 -0700

    wip

--- cli/qplat_api.py
+++ cli/qplat_api.py
@@ -1,6 +1,5 @@
 import datetime as dt
 import json
-import pprint
 
 def deposit(deposit_date, account, symbol, quantity):
     datetime = dt.datetime.strptime(deposit_date)
@@ -10,8 +9,13 @@ def deposit(deposit_date, account, symbol, quantity):
 def ingest(file_path):
     """ Ingest the given file """
     print "Ingesting: file: %s" % file_path
+
     with open(file_path) as data_file:
         data = json.load(data_file)
-        pprint.pprint(data)
-        data_file.close()
-    pass
+        ingest_items_list = data['ingest-items']
+        print "Ingest items list:\n", json.dumps(ingest_items_list, indent=4)
+        for item in ingest_items_list:
+            print item
+            print "DO SOMETHING HERE..."
+        pass
+
